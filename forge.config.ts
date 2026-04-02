ee220f7d9fb3067cf4552b86b6aba5eb4e62f820
brucewang <brucewang5638@gmail.com>
2026-04-02 15:23:11 +0800

    chore: set executableName to pretextchat in forge config

--- forge.config.ts
+++ forge.config.ts
@@ -10,6 +10,7 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 const config: ForgeConfig = {
   packagerConfig: {
     name: 'PretextChat',
+    executableName: 'pretextchat',
     asar: true,
   },
   rebuildConfig: {},
